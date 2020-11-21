378f169aa44f9e45b08e849d49dc1a8a4bae9460
genkot1 <gentaro.kotaka@gmail.com>
2020-11-21 17:14:17 +0900

    with_optionsの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   VALID_KANJI_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
   VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :kanji_last_name, format: { with: VALID_KANJI_REGEX, message:"full-width characters" }
+    validates :kanji_first_name, format: { with: VALID_KANJI_REGEX, message:"full-width characters" }
+    validates :kana_last_name, format: { with: VALID_KANA_REGEX, message:"full-width katakana characters" }
+    validates :kana_first_name, format: { with: VALID_KANA_REGEX, message:"full-width katakana characters" }
+    validates :birthday
+  end
+  
   validates :password, format: { with: VALID_PASSWORD_REGEX, message:"includes both letters and numbers" }
-  validates :kanji_last_name, presence: true, format: { with: VALID_KANJI_REGEX, message:"full-width characters" }
-  validates :kanji_first_name, presence: true, format: { with: VALID_KANJI_REGEX, message:"full-width characters" }
-  validates :kana_last_name, presence: true, format: { with: VALID_KANA_REGEX, message:"full-width katakana characters" }
-  validates :kana_first_name, presence: true, format: { with: VALID_KANA_REGEX, message:"full-width katakana characters" }
-  validates :birthday, presence: true
 end
