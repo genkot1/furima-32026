98e59a836a8dbffc58a5c5cb0adf23687c53e97f
genkot1 <gentaro.kotaka@gmail.com>
2020-11-23 23:27:15 +0900

    商品出品画面の金額計算以外の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,25 +2,25 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   
   def index
-    @items = Item.all
+    
   end
 
   def new
-    @item = Items.new
+    @item = Item.new
   end
 
   def create
-    @item = Items.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
-      redirect_to 
+      redirect_to root_path
     else
-      render 
+      render :new
     end
   end
 
   private
 
   def item_params
-    params.required(:item).permit(:name, :description, :shipping_fee_id, :category_id, :price, :prefecture_id, :duration_id).merge(user_id: current_user.id)
+    params.required(:item).permit(:image, :name, :description, :shipping_fee_id, :category_id, :price, :prefecture_id, :duration_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :duration
+
   has_one :purchase
   belongs_to :user
   has_one_attached :image
@@ -8,11 +15,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :shipping_fee_id
-    validates :category_id
-    validates :condition_id
-    validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }, format: {with: VALID_PRICE_REGEX}
-    validates :prefecture_id
-    validates :duration_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: {with: VALID_PRICE_REGEX}
+    with_options numericality: { other_than: 0 } do
+      validates :shipping_fee_id
+      validates :category_id
+      validates :condition_id
+      validates :prefecture_id
+      validates :duration_id
+    end
   end
 end
