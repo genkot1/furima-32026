d7f32324077ee4cadd0d67ab51ff1bf2a5ac7b4f
genkot1 <gentaro.kotaka@gmail.com>
2020-11-24 11:24:18 +0900

    validationの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,13 +13,14 @@ class Item < ApplicationRecord
   VALID_PRICE_REGEX = /\A[0-9]+\z/
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: {with: VALID_PRICE_REGEX}
-    with_options numericality: { other_than: 0 } do
-      validates :shipping_fee_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }, format: { with: VALID_PRICE_REGEX, message: "Half-width number" }
+    with_options numericality: { other_than: 0, message: "Select" } do
       validates :category_id
       validates :condition_id
+      validates :shipping_fee_id
       validates :prefecture_id
       validates :duration_id
     end
