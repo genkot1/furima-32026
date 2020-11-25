6c6ab1afc7507b1f243013dc0e3c61746c914cd7
genkot1 <gentaro.kotaka@gmail.com>
2020-11-25 18:44:39 +0900

    before_actionの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -38,12 +36,16 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.required(:item).permit(:image, :name, :description, :shipping_fee_id, :category_id, :condition_id, :price, :prefecture_id, :duration_id).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == Item.find(params[:id]).user.id
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
