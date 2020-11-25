e6900ee32bd12ab365d2b37b77d9bf0739413a8f
genkot1 <gentaro.kotaka@gmail.com>
2020-11-25 20:19:19 +0900

    投稿者本人のみが削除できる機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy, :move_to_index]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -35,7 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+    end
     redirect_to action: :index
   end
 
@@ -50,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == Item.find(params[:id]).user.id
       redirect_to action: :index
     end
   end
