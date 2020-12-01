4a45b7bc643c5d532fb3ede26370d49638d15d11
genkot1 <gentaro.kotaka@gmail.com>
2020-12-01 12:32:33 +0900

    テストコードの記述

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -1,13 +1,14 @@
 class PurchaseItem
   include ActiveModel::Model
-  attr_accessor :email, :nickname, :password, :kanji_last_name, :kanji_first_name, :kana_last_name, :kana_first_name, :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
     validates :city
     validates :house_number
-    validates :phone_number, length: {minimum: 10, maximum: 11, message: "Input only number" }
+    validates :phone_number, length: {minimum: 10, maximum: 11, message: "Input correctly" }
   end
+  validates :phone_number, format: { with: /\A[0-9]+\z/, message: "Input only number" }
   validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
 
   def save
---
+++ spec/models/purchase_item_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseItem, type: :model do
+  before do
+    @purchase_item = FactoryBot.build(:purchase_item)
+  end
+
+  describe '商品購入' do
+    context '商品購入がうまくいくとき' do
+      it 'すべての情報が正しく入力されていると商品が購入できる' do
+        expect(@purchase_item).to be_valid
+      end
+    end
+    context '商品購入がうまくいかないとき' do
+      it '郵便番号が空では購入できない' do
+        @purchase_item.postal_code = nil
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフンが入っていなければ購入できない' do
+        @purchase_item.postal_code = '0234567'
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it '都道府県が選択されていなければ購入できない' do
+        @purchase_item.prefecture_id = 0
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("Prefecture Select")
+      end
+      it '市区町村が入力されていなければ購入できない' do
+        @purchase_item.city = nil
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が入力されていなければ購入できない' do
+        @purchase_item.house_number = nil
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号が入力されていなければ購入できない' do
+        @purchase_item.phone_number = nil
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンが入っていると購入できない' do
+        @purchase_item.phone_number = "03-12345678"
+        @purchase_item.valid?
+        expect(@purchase_item.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
+end
